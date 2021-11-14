4c9acbeedd7ec927c6e28cac20310fac282ed51c
Kontsedal <kontsedalbohdan@gmail.com>
2021-11-14 13:30:45 +0200

    add state and ref utils

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import { Component } from "./renderer/renderer";
 
-const Tester: Component = ({ value }, { template }) => {
-  return template`<div><input type="range" min="0" max="10" value="${value}"/></div>`;
+const Tester2: Component = ({ value }, { template }) => {
+  return template`<div><input type="text" value="${value}"/></div>`;
+};
+const Tester: Component = ({ value }, { template, child }) => {
+  return template`<div><input type="range" min="0" max="10" value="${value}"/>${child(
+    Tester2,
+    { props: { value: 333 }, key: "inputText", dependencies: [] }
+  )}</div>`;
 };
 
 export const App: Component = (_, { template, child }) => {
--- src/renderer/renderer.ts
+++ src/renderer/renderer.ts
@@ -2,6 +2,8 @@ const CACHE_KEYS = {
   CHILDREN: "children",
   DEPENDENCIES: "deps",
   HTML: "html",
+  REFS: "refs",
+  STATES: "states",
 };
 type Utils = {
   template: (
@@ -16,6 +18,8 @@ type Utils = {
       key,
     }: { props: any; dependencies: any[]; key: string }
   ) => () => string;
+  ref: <T>(value: T) => { current: T };
+  state: <T>(value: T) => [state: T, setState: (newValue: T) => void];
 };
 
 export type Component = (props: any, utils: Utils) => string;
@@ -30,12 +34,9 @@ export const render = (
     props?: any;
     target?: HTMLElement;
   },
-  cache = new Map<string, any>()
+  cache = new Map<string | number, any>()
 ) => {
-  if (!cache.has(parentKey)) {
-    cache.set(parentKey, new Map());
-  }
-  const componentCache = cache.get(parentKey);
+  const componentCache = getSubCache(cache, parentKey);
   let child =
     (
       childComponent: Component,
@@ -43,7 +44,7 @@ export const render = (
         props,
         dependencies,
         key,
-      }: { props: any; dependencies: any[]; key: string }
+      }: { props?: any; dependencies: any[]; key: string }
     ) =>
     (): string => {
       let childKey = parentKey + "->" + key;
@@ -60,6 +61,31 @@ export const render = (
       childCache.set(CACHE_KEYS.DEPENDENCIES, dependencies);
       return html;
     };
+  let callCount = 0;
+  let ref = <T>(value: T): { current: T } => {
+    callCount++;
+    const refCache = getSubCache(cache, CACHE_KEYS.REFS);
+    if (refCache.has(callCount)) {
+      return refCache.get(callCount);
+    }
+    let result = { current: value };
+    refCache.set(callCount, result);
+    return result;
+  };
+  let state = <T>(value: T): [state: T, setState: (newValue: T) => void] => {
+    callCount++;
+    const statesCache = getSubCache(cache, CACHE_KEYS.REFS);
+    if (statesCache.has(callCount)) {
+      return statesCache.get(callCount);
+    }
+    const setState = (newValue: T) => {
+      let oldValue = statesCache.get(callCount);
+      statesCache.set(callCount, [newValue, oldValue[1]]);
+      performRender(true);
+    };
+    statesCache.set(callCount, [value, setState]);
+    return [value, setState];
+  };
   let template = (
     input: TemplateStringsArray,
     ...params: ReadonlyArray<any>
@@ -67,18 +93,33 @@ export const render = (
     return input.reduce((res, item, index) => {
       let entry = params[index];
       let param =
-        typeof entry === "function" ? entry(props, { template, child }) : entry;
+        typeof entry === "function" ? entry(props, getUtils()) : entry;
       return res + item + (param || "");
     }, "");
   };
 
-  const result = component(props, { template, child }).replace(
-    /^(<\w+)/,
-    `$1 data-key="${parentKey}"`
-  );
-  if (target) {
-    target.outerHTML = result;
+  function getUtils(): Utils {
+    return { template, child, ref, state };
   }
+  let componentRef: HTMLElement | null;
+  function performRender(isRerender: boolean) {
+    const result = component(props, getUtils()).replace(
+      /^(<\w+)/,
+      `$1 data-key="${parentKey}"`
+    );
+    if (target && !isRerender) {
+      target.outerHTML = result;
+    }
+    if (componentRef && isRerender) {
+      componentRef.outerHTML = result;
+      return result;
+    }
+    componentRef = document.querySelector(`[data-key="${parentKey}"]`);
+    return result;
+  }
+
+  let result = performRender(false);
+  callCount = 0;
   return result;
 };
 
@@ -89,13 +130,14 @@ function dependenciesAreEqual(prev: any[], current: any[]) {
   );
 }
 
-function getChildCache(cache: Map<string, any>, childKey: string) {
-  if (!cache.has(CACHE_KEYS.CHILDREN)) {
-    cache.set(CACHE_KEYS.CHILDREN, new Map());
+function getChildCache(cache: Map<string | number, any>, childKey: string) {
+  const childrenCache = getSubCache(cache, CACHE_KEYS.CHILDREN);
+  return getSubCache(childrenCache, childKey);
+}
+
+function getSubCache(cache: Map<string | number, any>, key: string) {
+  if (!cache.has(key)) {
+    cache.set(key, new Map());
   }
-  const childrenCache = cache.get(CACHE_KEYS.CHILDREN);
-  if (!childrenCache.has(childKey)) {
-    childrenCache.set(childKey, new Map());
-  }
-  return childrenCache.get(childKey);
+  return cache.get(key);
 }
